34b0ae67a4e2d6b78a3334d6b0ad38cb9c7f0e68
Hamza MADAD <hmadad@e1r1p6.42.fr>
2018-11-14 16:58:47 +0100

    (NORME) ft_malloc

--- srcs/ft_malloc.c
+++ srcs/ft_malloc.c
@@ -12,49 +12,57 @@
 
 #include "../includes/ft_malloc.h"
 
-t_base g_base;
+t_base	g_base;
 
-void    *ft_create_region(size_t len)
+void	*ft_create_region(size_t len)
 {
-    void *address;
-    int regionType;
+	void	*address;
+	int		region_type;
 
-    regionType = ft_get_type_region(len);
-    address = ft_allocate_memory(len, regionType);
-    if (get_list_region_length(regionType) == 0)
-        address = ft_create_new_list_region(len, (t_region *)address);
-    else
-        address = ft_select_region(regionType, len);
-    return (void*)address;
+	region_type = ft_get_type_region(len);
+	address = ft_allocate_memory(len, region_type);
+	if (get_list_region_length(region_type) == 0)
+		address = ft_create_new_list_region(len, (t_region *)address);
+	else
+		address = ft_select_region(region_type, len);
+	return ((void*)address);
 }
 
-void    *ft_allocate_memory(size_t len, size_t type)
+void	*ft_allocate_memory(size_t len, size_t type)
 {
-    void *address;
-    //size_t pageSize;
+	void	*address;
+	size_t	page_size;
 
-    //pageSize = getpagesize();
-    if (type == TINY_TYPE)
-        address = mmap(0, TINY_LENGTH, PROT_READ | PROT_WRITE, MAP_ANONYMOUS | MAP_PRIVATE, -1, 0);
-    else if (type == SMALL_TYPE)
-        address = mmap(0, SMALL_LENGTH, PROT_READ | PROT_WRITE, MAP_ANONYMOUS | MAP_PRIVATE, -1, 0);
-    else
-        address = mmap(0, (len + (PAGESIZE - (len % PAGESIZE))) * 100, PROT_READ | PROT_WRITE, MAP_ANONYMOUS | MAP_PRIVATE, -1, 0);
-
-    return address;
+	page_size = getpagesize();
+	if (type == TINY_TYPE)
+		address = mmap(0,
+				TINY_LENGTH,
+				PROT_READ | PROT_WRITE, MAP_ANONYMOUS | MAP_PRIVATE,
+				-1, 0);
+	else if (type == SMALL_TYPE)
+		address = mmap(0,
+				SMALL_LENGTH,
+				PROT_READ | PROT_WRITE, MAP_ANONYMOUS | MAP_PRIVATE,
+				-1, 0);
+	else
+		address = mmap(0,
+				(len + (page_size - (len % page_size))) * 100,
+				PROT_READ | PROT_WRITE, MAP_ANONYMOUS | MAP_PRIVATE,
+				-1, 0);
+	return (address);
 }
 
 void	*malloc(size_t len)
 {
-    void    *address;
+	void	*address;
 
-    address = NULL;
-    if (!g_base.called || g_base.called == 0)
-    {
+	address = NULL;
+	if (!g_base.called || g_base.called == 0)
+	{
 		g_base.called = 1;
-        address = ft_create_region(len);
-    }
-    else
-        address = ft_select_region(ft_get_type_region(len), len);
-	return (void*)address;
+		address = ft_create_region(len);
+	}
+	else
+		address = ft_select_region(ft_get_type_region(len), len);
+	return ((void*)address);
 }
