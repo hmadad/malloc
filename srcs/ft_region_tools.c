6e97cba85304a85ac72f190876acc61ce68c0000
Hamza MADAD <hmadad@e1r4p4.42.fr>
2018-11-15 11:31:14 +0100

    (NORME) ft_region_tools

--- srcs/ft_region_tools.c
+++ srcs/ft_region_tools.c
@@ -5,87 +5,88 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: hmadad <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2018/08/31 12:04:00 by hmadad            #+#    #+#             */
-/*   Updated: 2018/08/31 15:11:51 by hmadad           ###   ########.fr       */
+/*   Created: 2018/11/14 18:10:46 by hmadad            #+#    #+#             */
+/*   Updated: 2018/11/15 11:30:48 by hmadad           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/ft_malloc.h"
 
-t_base g_base;
+t_base		g_base;
 
-int     ft_get_type_region(size_t len)
+int			ft_get_type_region(size_t len)
 {
-    if (len < TINY_LIMIT)
-        return TINY_TYPE;
-    else if (len < SMALL_LIMIT && len >= TINY_LIMIT)
-        return SMALL_TYPE;
-    else
-        return LARGE_TYPE;
+	if (len < TINY_LIMIT)
+		return (TINY_TYPE);
+	else if (len < SMALL_LIMIT && len >= TINY_LIMIT)
+		return (SMALL_TYPE);
+	else
+		return (LARGE_TYPE);
 }
 
-void    *ft_select_region(size_t regionType, size_t len)
+void		*set_region_value(size_t region_type, t_region *region, size_t len)
 {
-    t_region    *region;
-    void        *address;
-    size_t      zone_header;
-    size_t      length_with_quantum;
+	size_t		quantum;
+	size_t		zone_header;
+	size_t		length_with_quantum;
+	void		*address;
 
-    address = NULL;
-    region = ft_search_region_place(regionType, len);
-    if (!region)
-    {
-        address = ft_allocate_memory(len, regionType);
-        address = ft_create_new_list_region(len, (t_region *)address);
-    }
-    else
-    {
-        if (regionType == TINY_TYPE)
-        {
-            zone_header = (sizeof(t_zone) + (TINY_QUANTUM_SIZE - (sizeof(t_zone) % TINY_QUANTUM_SIZE)));
-            length_with_quantum = (len + (TINY_QUANTUM_SIZE - (len % TINY_QUANTUM_SIZE)));
-        }
-        else if (regionType == SMALL_TYPE)
-        {
-            zone_header = (sizeof(t_zone) + (SMALL_QUANTUM_SIZE - (sizeof(t_zone) % SMALL_QUANTUM_SIZE)));
-            length_with_quantum = (len + (SMALL_QUANTUM_SIZE - (len % SMALL_QUANTUM_SIZE)));
-        }
-        else
-        {
-            zone_header = (sizeof(t_zone) + (LARGE_QUANTUM_SIZE - (sizeof(t_zone) % LARGE_QUANTUM_SIZE)));
-            length_with_quantum = (len + (LARGE_QUANTUM_SIZE - (len % LARGE_QUANTUM_SIZE)));
-        }
-        region->length = region->length - (zone_header + length_with_quantum);
-        address = ft_get_free_zone(region, length_with_quantum);
-        if (!address)
-        {
-            address = ft_allocate_memory(len, regionType);
-            address = ft_create_new_list_region(len, (t_region *)address);
-        }
-        else
-        {
-            address = ft_create_new_zone_list((t_zone *)address, length_with_quantum, zone_header);
-        }
-    }
-    return address;
+	address = NULL;
+	if (region_type == TINY_TYPE)
+		quantum = TINY_QUANTUM_SIZE;
+	else if (region_type == SMALL_TYPE)
+		quantum = SMALL_QUANTUM_SIZE;
+	else
+		quantum = LARGE_QUANTUM_SIZE;
+	zone_header = (sizeof(t_zone) + (quantum - (sizeof(t_zone) % quantum)));
+	length_with_quantum = (len + (quantum - (len % quantum)));
+	region->length = region->length - (zone_header + length_with_quantum);
+	address = ft_get_free_zone(region, length_with_quantum);
+	if (!address)
+	{
+		address = ft_allocate_memory(len, region_type);
+		address = ft_create_new_list_region(len, (t_region *)address);
+	}
+	else
+		address = ft_create_new_zone_list((t_zone *)address,
+				length_with_quantum, zone_header);
+	return (address);
 }
 
-t_region *ft_find_region(void * address)
+void		*ft_select_region(size_t region_type, size_t len)
 {
-    int i;
-    i = 0;
-    t_region *current;
+	t_region	*region;
+	void		*address;
 
-    while(i < 3)
-    {
-        current = g_base.tabList[i];
-        while (current)
-        {
-            if (address > (void *)current && address < ((void *)current + current->totalLength))
-                return current;
-            current = current->next;
-        }
-        i++;
-    }
-    return NULL;
+	address = NULL;
+	region = ft_search_region_place(region_type, len);
+	if (!region)
+	{
+		address = ft_allocate_memory(len, region_type);
+		address = ft_create_new_list_region(len, (t_region *)address);
+	}
+	else
+		address = set_region_value(region_type, region, len);
+	return (address);
+}
+
+t_region	*ft_find_region(void *address)
+{
+	int			i;
+	t_region	*current;
+
+	i = 0;
+	while (i < 3)
+	{
+		current = g_base.tabList[i];
+		while (current)
+		{
+			if (address > (void *)current
+					&& address < ((void *)current + current->totalLength))
+				return (current);
+			current = current->next;
+		}
+		i++;
+	}
+	return (NULL);
 }
